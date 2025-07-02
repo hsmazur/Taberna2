fd8b638c386680d9f9ab4d163232dfd1c51abb02
hsmazur <hugomazur16@gmail.com>
2025-07-02 10:58:57 -0300

    atualizacao 02/07 11:00

--- frontend/js/class.js
+++
@@ -1,8 +0,0 @@
-class Lanche {
-    constructor(imagem, nome, ingredientes, preco) {
-        this.imagem = imagem;
-        this.nome = nome;
-        this.ingredientes = ingredientes;
-        this.preco = preco;
-    }
-}
